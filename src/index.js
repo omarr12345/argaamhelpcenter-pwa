f4da94dbd004b987bbb35129c88b17a75ab0d003
Omar Mostafa <omar.mostafa@argaam.com>
2023-01-17 11:12:24 +0200

    adding pwa

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
+// ** Import Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// ** Import Browser Router
 import { BrowserRouter } from "react-router-dom";
-import swDev from "./swDev";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
-
-swDev();
+serviceWorkerRegistration.register();
+reportWebVitals();
